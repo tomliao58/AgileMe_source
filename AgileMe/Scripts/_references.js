86e365d2cddd2606f18500425f2942ac124e2894
tomliao58@gmail.com <cbb/38;oenny>
2016-02-19 20:32:26 +0800

    AgileMe Web - master

---
+++ AgileMe/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../js/jquery.animate-colors-min.js" />
+/// <reference path="../js/jquery.easing.1.3.js" />
+/// <reference path="../js/jquery.skitter.min.js" />
+/// <reference path="../js/jquery-2.1.1.min.js" />
+/// <reference path="../SpryAssets/SpryMenuBar.js" />
